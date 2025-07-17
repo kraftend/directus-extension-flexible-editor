9fba533dff497eb47969596c3a79874a376628de
cemreinanc <cemreinanc@gmail.com>
2025-07-17 23:27:59 +0200

    Update base dependencies and remove content files

--- content/vue/types.ts
+++
@@ -1,9 +0,0 @@
-export type * from "tiptap-render-view/vue";
-import type { Component } from "vue";
-import type { RelationNodeSerializers } from "../types";
-
-export type * from "../types";
-export type VueRelationNodeSerializers = RelationNodeSerializers<Component>;
-
-// TODO: [Stage 2][deprecated] type VueRelationBlockSerializers
-export type VueRelationBlockSerializers = VueRelationNodeSerializers;
